1b65d0c310d3beedab434c1ced09128c6b18fc05
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-09-24 19:35:37 +0200

    💐 Config is now reloaded without restarting the game

--- src/main/java/dev/xylonity/explosiveenhancement/event/ExplosiveModEvents.java
+++ src/main/java/dev/xylonity/explosiveenhancement/event/ExplosiveModEvents.java
@@ -1,9 +1,11 @@
 package dev.xylonity.explosiveenhancement.event;
 
 import dev.xylonity.explosiveenhancement.ExplosiveEnhancement;
+import dev.xylonity.explosiveenhancement.config.ExplosiveValues;
 import dev.xylonity.explosiveenhancement.network.ExplosiveNetwork;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = ExplosiveEnhancement.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -12,6 +14,16 @@ public class ExplosiveModEvents {
     @SubscribeEvent
     public static void onCommonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(ExplosiveNetwork::register);
+    }
+
+    @SubscribeEvent
+    public static void onConfigLoad(final ModConfigEvent.Loading event) {
+        ExplosiveValues.onConfigLoad(event);
+    }
+
+    @SubscribeEvent
+    public static void onConfigReload(final ModConfigEvent.Reloading event) {
+        ExplosiveValues.onConfigLoad(event);
     }
 
 }
